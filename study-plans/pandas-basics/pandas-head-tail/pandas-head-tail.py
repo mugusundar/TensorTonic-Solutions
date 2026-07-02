cda4411c8c34caa4baae3e0e02d940b042b0e4b7
Mugunthan Sundararaju <35947960+mugusundar@users.noreply.github.com>
2026-07-02 07:40:59 +0530

    Solve pandas-head-tail

---
+++ study-plans/pandas-basics/pandas-head-tail/pandas-head-tail.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def head_tail(data, n):
+    """
+    Returns: dict with 'head' and 'tail' (both dicts of column -> list)
+    """
+    df = pd.DataFrame(data)
+
+    return {
+        "head" : df.head(n).to_dict(orient="list"),
+        "tail" : df.tail(n).to_dict(orient="list")
+    }
